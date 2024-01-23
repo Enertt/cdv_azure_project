9a7f1a4f9fa79cd393da980c59e19b07d2df8aa8
ytsipak <ytsipak@gmail.com>
2024-01-23 21:56:35 +0100

    Header & Main

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,32 +1,46 @@
-import s from './main.module.css'
-import logoImg from '../../assets/images/ZOO LOGO.png'
+import s from "./main.module.css";
+import logoImg from "../../assets/images/ZOO LOGO.png";
 
 const Main = () => {
-    return(
-        <div className={s.wrapper}>
-            <header className={s.header}>
-                <div className={s.logoBlock}>
-                    <img src={logoImg}></img>
-                </div>
-                <div className={s.navBlock}>
-                    <nav>
-                        <span>ABOUT US</span>
-                        <span>ANIMALS</span>
-                        <span>PRICES</span>
-                        <button className={s.contactButton}>
-                            <span>CONTACT US</span>
-                        </button>
-                    </nav>
-                </div>
-            </header>
-            <div className={s.mainBlock}>
-                <div className={s.infoBlock}>
-                    <span>Przyjdź zobaczyć zwierzęta w naszym zoo</span>
-                    <button>Bilety</button>
-                </div>
-            </div>
+  return (
+    <>
+      <header className={s.header}>
+        <div className={s.container}>
+          <nav className={s.headerNav}>
+            <a href="/">
+              <img src={logoImg}></img>
+            </a>
+            <ul className={s.headerMenu}>
+              <li>
+                <a className={s.headerLink}>About us</a>
+              </li>
+              <li>
+                <a className={s.headerLink}>Animals</a>
+              </li>
+              <li>
+                <a className={s.headerLink}>Prices</a>
+              </li>
+              <button>CONTACT US</button>
+            </ul>
+          </nav>
         </div>
-    )
-}
+      </header>
+      <section className={s.heroSection}>
+        {/* <div className={s.container}> */}
+        {/* <div className={s.mainBlock}>
+            <div className={s.infoBlock}> */}
+        <div className={s.heroContainer}>
+          <h1 className={s.heroSectionTitle}>
+            Przyjdź zobaczyć zwierzęta <br />w naszym zoo
+          </h1>
+          <button className={s.heroSectionButton}>Bilety</button>
+          {/* </div>
+          </div> */}
+          {/* </div> */}
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Main
+export default Main;
